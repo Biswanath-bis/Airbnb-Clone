fb91bdbc811ed3b5fee407c08e89ec6efeb3a943
Biswanath bhuyan <info.biswanath.bhuyan@gmail.com>
2025-07-07 15:03:23 +0530

    initial commit

--- init/index.js
+++ init/index.js
@@ -3,7 +3,8 @@ const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 const User = require("../models/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=ATLASDB_URL;
 
 main()
   .then(() => {
@@ -14,7 +15,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
